3c6ab3eea0fff5efb7d49018ab70e499f0a14eb5
phenix1229 <paul.garay@codeimmersives.com>
2019-11-20 20:57:22 -0500

    achieved basic functionality and increased canvas size

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
-function changeColor(color){
-    event.target.
+for(let i = 0; i < 4000; i++){
+    const div = document.createElement('div');
+    div.className = "square";
+    div.style.width = '10px';
+    div.style.height = "10px";
+    document.querySelector('.canvas').appendChild(div);
 }
 
-document.querySelector('.palette-color').addEventListener('click', )
+const brush = document.querySelector('.current-brush');
+const canvas = document.querySelector('.canvas');
+const canvasSquares = document.querySelectorAll('.square');
+const colorCounter = {
+    'color-1': 0,
+}
+
+function changeBrushColor(){
+    brush.classList.remove(brush.classList.item(1));
+    brush.classList.add(event.target.classList.item(1));
+}
+
+function changeColor(){
+    event.target.classList.remove(event.target.classList.item(1));
+    event.target.classList.add(brush.classList.item(1));
+}
+
+document.querySelectorAll('.palette-color').forEach(color => {color.addEventListener('click', changeBrushColor)});
+
+
+
+canvas.style.gridTemplateColumns = 'repeat(80, 10px)';
+canvas.style.gridTemplateRows = 'repeat(50, 10px)';
+canvasSquares.forEach(square => {square.addEventListener('click', changeColor)});
